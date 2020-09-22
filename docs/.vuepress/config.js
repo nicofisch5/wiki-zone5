e3bbfbc76ac5be49b14d85d0cddd70becc953a03
Nicolas Fischer <fischer@happysoft.fr>
2020-09-22 19:23:36 +0200

    Add sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,15 @@
 module.exports = {
   title: 'Wiki Zone 5',
-  description: 'My WiKi from dokuwiki to vuepress'
+  description: 'My WiKi from dokuwiki to vuepress',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Github', link: 'https://github.com/' }
+    ],
+    sidebar: [
+      { title: "Home", children: [""] },
+    ],
+    lastUpdated: 'Last Updated', // string | boolean
+    displayAllHeaders: true,
+  }
 }
